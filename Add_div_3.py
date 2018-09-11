aa003f8e2d72858886652f7ec6e5331266072de7
Matt Laptop <mdguptil@gmail.com>
2018-09-11 16:08:26 -0400

    nitial Commit

---
+++ Add_div_3.py
@@ -0,0 +1,19 @@
+def main():
+	holder = addthree(1,2,3)
+	print(div(holder))
+	
+
+def addthree(v1,v2,v3):
+	return v1 + v2 +v3
+
+
+def div(v1):
+	return v1 /3
+
+
+
+
+if __name__ == '__main__':
+	
+	main()
+
